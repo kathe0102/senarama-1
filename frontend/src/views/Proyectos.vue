8bdec7b73664178714705c307373d9479f685c2f
Devil64-Dev <devil64dev@gmail.com>
2021-11-04 10:33:30 -0500

    [views/Proyectos.vue] updated videos and section navigation position

--- frontend/src/views/Proyectos.vue
+++ frontend/src/views/Proyectos.vue
@@ -2,16 +2,6 @@
   <main>
     <div class="project-container">
 
-      <!-- entries navigation -->
-      <div class="project-nav">
-        <nav>
-          <a href="#entry-1">AUDIOVISUALES</a>
-          <a href="#entry-2">MÚSICA</a>
-          <a href="#entry-3">ARTES ESCENICAS</a>
-          <a href="#entry-4">MULTIMEDIA</a>
-        </nav>
-      </div>
-
       <!-- entry with dark background -->
       <div class="project-entry background-dark" id="entry-1">
         <h2 class="entry-title">Audiovisuales</h2>
@@ -19,7 +9,7 @@
         <div class="entries-container">
           <div class="entry-video entry-left">
             <iframe
-              src="https://www.youtube.com/embed/V98XE_VKN-0">
+              src="https://drive.google.com/file/d/1O14sN6XxDyVcev-HMS24iBObjv-7Tz16/preview">
             </iframe>
           </div>
           <!-- images at center of entry -->
@@ -30,7 +20,7 @@
           </div>
           <!-- video at right of entry -->
           <div class="entry-video entry-right">
-            <iframe src="https://www.youtube.com/embed/ioMaaWrPh3g">
+            <iframe src="https://drive.google.com/file/d/1RutYwKf95ZIxQ6tgIU40lSlAZcF0ZkR1/preview">
             </iframe>
           </div>
         </div>
@@ -42,7 +32,7 @@
         <div class="entries-container">
           <div class="entry-video entry-left">
             <iframe
-              src="https://www.youtube.com/embed/V98XE_VKN-0">
+              src="https://drive.google.com/file/d/1KSgv4-MhA_Nj9W69lCS0PTnah0aRmz2M/preview">
             </iframe>
           </div>
           <!-- images at center of entry -->
@@ -53,7 +43,7 @@
           </div>
           <!-- video at right of entry -->
           <div class="entry-video entry-right">
-            <iframe src="https://www.youtube.com/embed/ioMaaWrPh3g">
+            <iframe src="https://drive.google.com/file/d/18itG0gDp5wAVctk2tSN4vKvn6wfE7LZs/preview">
             </iframe>
           </div>
         </div>
@@ -65,7 +55,7 @@
         <div class="entries-container">
           <div class="entry-video entry-left">
             <iframe
-              src="https://www.youtube.com/embed/V98XE_VKN-0">
+              src="https://drive.google.com/file/d/159RAvv815185g8CiknrXWjfthBZJJ7eg/preview">
             </iframe>
           </div>
           <!-- images at center of entry -->
@@ -76,7 +66,7 @@
           </div>
           <!-- video at right of entry -->
           <div class="entry-video entry-right">
-            <iframe src="https://www.youtube.com/embed/ioMaaWrPh3g">
+            <iframe src="https://drive.google.com/file/d/1R6h14SuuREFsmOK_tuqwikw3qhu4mhh3/preview">
             </iframe>
           </div>
         </div>
@@ -88,7 +78,7 @@
         <div class="entries-container">
           <div class="entry-video entry-left">
             <iframe
-              src="https://www.youtube.com/embed/V98XE_VKN-0">
+              src="https://drive.google.com/file/d/1hn69Gn1SgHLwlhYmMy4pTn86CqIMpBqg/preview">
             </iframe>
           </div>
           <!-- images at center of entry -->
@@ -103,6 +93,16 @@
             </iframe>
           </div>
         </div>
+      </div>
+
+      <!-- entries navigation -->
+      <div class="project-nav">
+        <nav>
+          <a href="#entry-1">AUDIOVISUALES</a>
+          <a href="#entry-2">MÚSICA</a>
+          <a href="#entry-3">ARTES ESCENICAS</a>
+          <a href="#entry-4">MULTIMEDIA</a>
+        </nav>
       </div>
 
     </div>
