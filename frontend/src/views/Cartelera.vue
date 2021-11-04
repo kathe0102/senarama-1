71e048c3c9776ca91b24ee33a4980f2ac8667428
Devil64-Dev <devil64dev@gmail.com>
2021-11-04 11:48:30 -0500

    [views/Cartelera.vue] updated content

--- frontend/src/views/Cartelera.vue
+++ frontend/src/views/Cartelera.vue
@@ -1,69 +1,67 @@
 <template>
-<main>
-  <div>
-       <div class="row">
-                <br>
-                <a><h3>DESTACADOS</h3></a>
-                <br>
-                <iframe width="400" height="315" src="https://www.youtube.com/embed/Ik-7cQkvCFw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                                        
-                <iframe width="400" height="200" src="https://www.youtube.com/embed/7001BUNgGfA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe width="400" height="200" src="https://www.youtube.com/embed/fLsn2CA8mVs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                
-                <br>
-        </div>
-    
-        
-            <div class="row">
-                <br>
-                <a><h3>AUDIOVISUALES</h3></a>
-                <br>
-                <iframe width="400" height="315" src="https://www.youtube.com/embed/EP5sKuMbio4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe width="400" height="200" src="https://www.youtube.com/embed/Q1otLLQWDLA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                
-                <iframe width="400" height="200" src="https://www.youtube.com/embed/s_YJA9JQ_nM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <br>
-            </div>
-          
-       
-            <div class="row">
-                <br>
-                <a><h3>MUSICA</h3></a>
-                <br>
-                <iframe width="400" height="315" src="https://www.youtube.com/embed/dIKJ66glp_g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe width="400" height="200" src="https://www.youtube.com/embed/7001BUNgGfA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe width="400" height="200" src="https://www.youtube.com/embed/njdHE9UeYzY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <br>
-            </div>
-       
-            <div class="row">
-                <br>
-                <a><h3>ARTES ESCENICAS</h3></a>
-                <br>
-                <iframe width="400" height="200" src="https://www.youtube.com/embed/fLsn2CA8mVs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe width="400" height="200" src="https://www.youtube.com/embed/k5rZk-qC0JM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe width="400" height="200" src="https://www.youtube.com/embed/NKAsPzeDEXA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <br>
-            </div>
+  <main>
+    <div class="billboard-container">
+      <div class="billboard-entry border-orange">
+          <h3 class="entry-title">DESTACADOS</h3>
+          <div class="entries-container">
+              <div class="entry-video entry-left">
+                <iframe src="https://www.youtube.com/embed/Ik-7cQkvCFw"></iframe>
+              </div>
+              <div class="entry-video entry-center">
+                <iframe src="https://www.youtube.com/embed/fLsn2CA8mVs"></iframe>
+              </div>
+              <div class="entry-video entry-right">
+                <iframe src="https://www.youtube.com/embed/7001BUNgGfA"></iframe>
+              </div>
+          </div>
+      </div>
+
+      <div class="billboard-entry border-green">
+          <h3 class="entry-title">AUDIOVISUALES</h3>
+          <div class="entries-container">
+              <div class="entry-video entry-left">
+                <iframe src="https://www.youtube.com/embed/EP5sKuMbio4"></iframe>
+              </div>
+              <div class="entry-video entry-center">
+                <iframe src="https://www.youtube.com/embed/Q1otLLQWDLA"></iframe>
+              </div>
+              <div class="entry-video entry-right">
+                <iframe src="https://www.youtube.com/embed/s_YJA9JQ_nM"></iframe>
+              </div>
+          </div>
+      </div>
+
+      <div class="billboard-entry border-green">
+          <h3 class="entry-title">MUSICA</h3>
+          <div class="entries-container">
+              <div class="entry-video entry-left">
+                <iframe src="https://www.youtube.com/embed/dIKJ66glp_g"></iframe>
+              </div>
+              <div class="entry-video entry-center">
+                <iframe src="https://www.youtube.com/embed/7001BUNgGfA"></iframe>
+              </div>
+              <div class="entry-video entry-right">
+                <iframe src="https://www.youtube.com/embed/njdHE9UeYzY"></iframe>
+              </div>
+          </div>
+      </div>
+
+      <div class="billboard-entry border-orange">
+          <h3 class="entry-title">ARTES ESCENICAS</h3>
+          <div class="entries-container">
+              <div class="entry-video entry-left">
+                <iframe src="https://www.youtube.com/embed/fLsn2CA8mVs"></iframe>
+              </div>
+              <div class="entry-video entry-center">
+                <iframe src="https://www.youtube.com/embed/k5rZk-qC0JM"></iframe>
+              </div>
+              <div class="entry-video entry-right">
+                <iframe src="https://www.youtube.com/embed/NKAsPzeDEXA"></iframe>
+              </div>
+          </div>
+      </div>
     </div>
-   
-</main>
+  </main>
 </template>
-<script>
 
-
-</script>
-
-<style scoped lang='scss'>
-main{
- 
-    padding-top: 12rem;
-    width: 100%;
-    margin-top: 200px;
-    display:flex;
-    flex-direction:row;
-    flex-wrap: rap;
-    justify-content: space-between;
-    
-    flex-grow: 1;
-    padding-top: 1rem;
-}
-
-</style>
+<style scoped src="../assets/styles/billboard.css"></style>
