b3434c43c0f992bc29f8282e05f51491044f6ba7
Devil64-Dev <devil64dev@gmail.com>
2021-11-04 10:20:24 -0500

    [views/Proyectos.vue] added new content

--- frontend/src/views/Proyectos.vue
+++ frontend/src/views/Proyectos.vue
@@ -1,5 +1,113 @@
 <template>
-  <div class="container">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <div class="project-container">
+
+      <!-- entries navigation -->
+      <div class="project-nav">
+        <nav>
+          <a href="#entry-1">AUDIOVISUALES</a>
+          <a href="#entry-2">MÚSICA</a>
+          <a href="#entry-3">ARTES ESCENICAS</a>
+          <a href="#entry-4">MULTIMEDIA</a>
+        </nav>
+      </div>
+
+      <!-- entry with dark background -->
+      <div class="project-entry background-dark" id="entry-1">
+        <h2 class="entry-title">Audiovisuales</h2>
+        <!-- video at left of entry -->
+        <div class="entries-container">
+          <div class="entry-video entry-left">
+            <iframe
+              src="https://www.youtube.com/embed/V98XE_VKN-0">
+            </iframe>
+          </div>
+          <!-- images at center of entry -->
+          <div class="entry-images entry-center">
+            <img src="../assets/images/17.jpg" class="image-grid">
+            <img src="../assets/images/20.jpg" class="image-grid">
+            <img src="../assets/images/27.jpg" class="image-grid">
+          </div>
+          <!-- video at right of entry -->
+          <div class="entry-video entry-right">
+            <iframe src="https://www.youtube.com/embed/ioMaaWrPh3g">
+            </iframe>
+          </div>
+        </div>
+      </div>
+
+      <div class="project-entry" id="entry-2">
+        <h2 class="entry-title">Música</h2>
+        <!-- video at left of entry -->
+        <div class="entries-container">
+          <div class="entry-video entry-left">
+            <iframe
+              src="https://www.youtube.com/embed/V98XE_VKN-0">
+            </iframe>
+          </div>
+          <!-- images at center of entry -->
+          <div class="entry-images entry-center">
+            <img src="../assets/images/7.jpg" class="image-grid">
+            <img src="../assets/images/25.jpg" class="image-grid">
+            <img src="../assets/images/9.jpg" class="image-grid">
+          </div>
+          <!-- video at right of entry -->
+          <div class="entry-video entry-right">
+            <iframe src="https://www.youtube.com/embed/ioMaaWrPh3g">
+            </iframe>
+          </div>
+        </div>
+      </div>
+
+      <div class="project-entry background-dark" id="entry-3">
+        <h2 class="entry-title">Artes Escenicas</h2>
+        <!-- video at left of entry -->
+        <div class="entries-container">
+          <div class="entry-video entry-left">
+            <iframe
+              src="https://www.youtube.com/embed/V98XE_VKN-0">
+            </iframe>
+          </div>
+          <!-- images at center of entry -->
+          <div class="entry-images entry-center">
+            <img src="../assets/images/6.jpg" class="image-grid">
+            <img src="../assets/images/12.jpg" class="image-grid">
+            <img src="../assets/images/16.jpg" class="image-grid">
+          </div>
+          <!-- video at right of entry -->
+          <div class="entry-video entry-right">
+            <iframe src="https://www.youtube.com/embed/ioMaaWrPh3g">
+            </iframe>
+          </div>
+        </div>
+      </div>
+
+      <div class="project-entry" id="entry-4">
+        <h2 class="entry-title">Multimedia</h2>
+        <!-- video at left of entry -->
+        <div class="entries-container">
+          <div class="entry-video entry-left">
+            <iframe
+              src="https://www.youtube.com/embed/V98XE_VKN-0">
+            </iframe>
+          </div>
+          <!-- images at center of entry -->
+          <div class="entry-images entry-center">
+            <img src="../assets/images/2.jpg" class="image-grid">
+            <img src="../assets/images/senaApp.jpg" class="image-grid">
+            <img src="../assets/images/senaPC.jpg" class="image-grid">
+          </div>
+          <!-- video at right of entry -->
+          <div class="entry-video entry-right">
+            <iframe src="https://www.youtube.com/embed/ioMaaWrPh3g">
+            </iframe>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </main>
 </template>
+
+<style scoped src="../assets/styles/proyectos_style.css">
+</style>
